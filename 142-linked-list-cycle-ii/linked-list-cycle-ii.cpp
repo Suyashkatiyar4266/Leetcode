f66dabe2d8043a35ad63ce36931522446ad261f5
Suyash Katiyar <2k24.csai1d.2410105@gmail.com>
2026-07-23 10:47:52 +0530

    Time: 10 ms (45.79%) | Memory: 11.3 MB (86.64%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,26 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(nullptr) {}
- * };
- */
-
+//Hare and tortoise approach(Slow and fast approach)
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> visited;
+        if (head == NULL || head->next == NULL)
+            return NULL;
 
-        while (head != NULL) {
-            if (visited.find(head) != visited.end()) {
-                return head;      // Start of the cycle
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        // Step 1: Detect cycle
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                // Step 2: Find starting node of cycle
+                ListNode *start = head;
+
+                while (start != slow) {
+                    start = start->next;
+                    slow = slow->next;
+                }
+
+                return start;
             }
-
-            visited.insert(head);
-            head = head->next;
         }
 
-        return NULL;   // No cycle
+        return NULL;
     }
 };
