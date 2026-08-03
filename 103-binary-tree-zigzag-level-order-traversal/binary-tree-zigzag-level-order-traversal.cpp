7cdaba2c4ca8e559d077651e5295fa649acd2b76
Suyash Katiyar <2k24.csai1d.2410105@gmail.com>
2026-08-03 10:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (48.90%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,43 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
 
         vector<vector<int>> ans;
 
-        if(root == NULL)
+        if (root == NULL)
             return ans;
 
         queue<TreeNode*> q;
         q.push(root);
 
-        bool leftToRight = true;
+        bool flag = false;   // false = Left -> Right
+                             // true  = Right -> Left
 
-        while(!q.empty()) {
+        while (!q.empty()) {
 
             int size = q.size();
-
-            // Change 1
             vector<int> level(size);
 
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
 
                 TreeNode* node = q.front();
                 q.pop();
 
-                // Change 2
-                int index = leftToRight ? i : size - 1 - i;
-                level[index] = node->val;
+                if (flag)
+                    level[size - 1 - i] = node->val;
+                else
+                    level[i] = node->val;
 
-                if(node->left)
+                if (node->left)
                     q.push(node->left);
 
-                if(node->right)
+                if (node->right)
                     q.push(node->right);
             }
 
             ans.push_back(level);
 
-            leftToRight = !leftToRight;
+            flag = !flag;
         }
 
         return ans;
