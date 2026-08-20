505b84d4334bf39d1b34adcc1eb86f9e44157774
Suyash Katiyar <2k24.csai1d.2410105@gmail.com>
2026-08-20 10:45:49 +0530

    Time: 0 ms (100.00%) | Memory: 33.4 MB (40.09%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -9,18 +9,73 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+//         // if(root1==NULL){
+//         //     return root2;
+//         // }
+//         // if(root2==NULL){
+//         //     return root1;
+//         // }
+//         // root1->val = root1->val + root2->val;
+//         // root1->left = mergeTrees(root1->left , root2->left);
+//         // root1->right = mergeTrees(root1->right , root2->right);
+
+//         // return root1;
+
+
+        
+
+
+//     }
+// };
+
+
+                  //Iterative Approach using Stack 
+
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1==NULL){
+        
+        if (root1 == NULL) {
             return root2;
         }
-        if(root2==NULL){
+        
+        if (root2 == NULL) {
             return root1;
         }
-        root1->val = root1->val + root2->val;
-        root1->left = mergeTrees(root1->left , root2->left);
-        root1->right = mergeTrees(root1->right , root2->right);
+
+        stack<pair<TreeNode*, TreeNode*>> st;
+        st.push({root1, root2});
+
+        while (!st.empty()) {
+            
+            pair<TreeNode*, TreeNode*> p = st.top();
+            st.pop();
+
+            TreeNode* t1 = p.first;
+            TreeNode* t2 = p.second;
+
+            // Both nodes are guaranteed to be non-null here
+            t1->val = t1->val + t2->val;
+
+            // Left subtree
+            if (t1->left == NULL) {
+                t1->left = t2->left;
+            }
+            else if (t2->left != NULL) {
+                st.push({t1->left, t2->left});
+            }
+
+            // Right subtree
+            if (t1->right == NULL) {
+                t1->right = t2->right;
+            }
+            else if (t2->right != NULL) {
+                st.push({t1->right, t2->right});
+            }
+        }
 
         return root1;
     }
