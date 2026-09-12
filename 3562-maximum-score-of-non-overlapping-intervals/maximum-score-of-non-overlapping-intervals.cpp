0d2646e611ed9d0bcdfdc0569815d508fd2e14e1
Suyash Katiyar <2k24.csai1d.2410105@gmail.com>
2026-09-12 22:39:39 +0530

    Time: 801 ms (64.13%) | Memory: 272.1 MB (71.74%) - LeetSync

--- 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.cpp
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.cpp
@@ -1,52 +1,28 @@
 class Solution {
-    struct IntervalData {
-        int end, start, weight, originalIndex;
-        // Used for sorting and lower_bound comparisons
-        bool operator<(const IntervalData& other) const {
-            return end < other.end;
-        }
-    };
-
 public:
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        vector<IntervalData> sortedIntervals;
-        
-        for (int i = 0; i < n; ++i) {
-            sortedIntervals.push_back({intervals[i][1], intervals[i][0], intervals[i][2], i});
-        }
-        sort(sortedIntervals.begin(), sortedIntervals.end());
-        
-        // dp[i][j] stores a pair: {-max_weight, lexicographically_smallest_indices}
-        vector<vector<pair<long long, vector<int>>>> dp(n + 1, vector<pair<long long, vector<int>>>(5, {0LL, {}}));
-        
-        for (int i = 0; i < n; ++i) {
-            int start = sortedIntervals[i].start;
-            int weight = sortedIntervals[i].weight;
-            int originalIndex = sortedIntervals[i].originalIndex;
-            
-            // lower_bound finds the first interval whose end >= current start
-            IntervalData target = {start, 0, 0, 0}; 
-            int k = lower_bound(sortedIntervals.begin(), sortedIntervals.begin() + i, target) - sortedIntervals.begin();
-            
-            for (int j = 1; j <= 4; ++j) {
-                long long prevWeight = dp[k][j - 1].first;
-                vector<int> prevIndices = dp[k][j - 1].second;
-                
-                pair<long long, vector<int>> skip = dp[i][j];
-                
-                vector<int> takeIndices = prevIndices;
-                takeIndices.push_back(originalIndex);
-                sort(takeIndices.begin(), takeIndices.end());
-                
-                pair<long long, vector<int>> take = {prevWeight - weight, takeIndices};
-                
-                // min() naturally prioritizes the lowest (most negative) weight sum, 
-                // then lexicographically smallest sorted indices
-                dp[i + 1][j] = min(skip, take);
+        vector<int> order(n);
+        iota(order.begin(), order.end(), 0);
+        sort(order.begin(), order.end(), [&](int a, int b) { return intervals[a][1] < intervals[b][1]; });
+        vector<int> rights(n);
+        for (int p = 0; p < n; ++p) rights[p] = intervals[order[p]][1];
+
+        using State = pair<long long, vector<int>>;
+        vector<State> prev(n + 1, {0, {}});  // k = 0: nothing picked
+        for (int k = 0; k < 4; ++k) {
+            vector<State> cur(n + 1, {0, {}});
+            for (int p = 1; p <= n; ++p) {
+                int i = order[p - 1];  // take next interval
+                int l = intervals[i][0], w = intervals[i][2];
+                int j = lower_bound(rights.begin(), rights.end(), l) - rights.begin();  // intervals ending before l
+                State take = prev[j];
+                take.first -= w;  // scores kept negative
+                take.second.insert(upper_bound(take.second.begin(), take.second.end(), i), i);
+                cur[p] = min(take, cur[p - 1]);  // min = best score, then lex smallest
             }
+            prev = move(cur);
         }
-        
-        return dp[n][4].second;
+        return prev[n].second;
     }
 };
